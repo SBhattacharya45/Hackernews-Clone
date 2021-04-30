f9d8f9976e01765c09a7d27acdcfe03a385b0a90
SBhattacharya45 <umyadeepb27@gmail.com>
2021-04-30 17:38:24 +0530

    added top header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../Assets/logo_top.png';
+
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={classes.Headerdiv}>
+            <img src={logo} />
+        </div>
+    )
+}
+
+export default Header;
