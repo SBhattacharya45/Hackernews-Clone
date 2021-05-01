f4e78aea19242f03ed89bdfbf54a36716df2b222
SBhattacharya45 <umyadeepb27@gmail.com>
2021-05-01 21:38:14 +0530

    removed all console.logs

--- src/Components/NewPosts/NewPosts.js
+++ src/Components/NewPosts/NewPosts.js
@@ -16,28 +16,11 @@ const NewPosts = () => {
     useEffect( () => {
 
         const fetchData = async () => {
-            console.log("fetching posts");
             const result = await axios.get("https://hacker-news.firebaseio.com/v0/newstories.json")
             setPosts(result.data);
         }
 
         fetchData();
-
-    //   const res = await axios.get('https://hacker-news.firebaseio.com/v0/item/26993857.json?print=pretty');
-    //   console.log(res);
-        // let newDetails = details;
-        // let count = 0;
-        // posts.slice(0, 5).forEach(async post => {
-        //     const res = await axios.get('https://hacker-news.firebaseio.com/v0/item/26993857.json?print=pretty');
-        //     console.log(res);
-        //     newDetails.push(res);
-        //     count++;
-        //     if(count === 5){
-        //         console.log("ok");
-        //         setFlag(true);
-        //         setDetails(newDetails);
-        //     }
-        // })
     }, []);
 
     useEffect( () => {
@@ -49,11 +32,9 @@ const NewPosts = () => {
                 let postsList = null;
                 posts.slice(0,post_count).forEach( async id => {
                     const res = await axios.get('https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty');
-                    // console.log(res);
                     newDetails.push(res.data);
                     count++;
                     if(count === post_count){
-                        // console.log(details);
                         postsList = newDetails.map((data, igKey) =>{
                             return(
                                 <Post key={igKey} title={data.title} link={data.url}/>
@@ -66,38 +47,27 @@ const NewPosts = () => {
                 })
             }
 
-            // console.log(posts.slice(0, post_count));
-            // console.log("fetched posts");
             setData();
           
         }
     }, [posts])
 
     const onLoadHandler = async () => {
-        // console.log("setLoader");
         const container = document.querySelector('#postContainer')
         setLoader(true);
         let newList = list;
         let newDetails = [];
         let count = 0;
-        console.log(newList);
-        console.log("set count to zero");
         posts.slice(offset,(post_count + offset)).forEach( async id => {
             const res = await axios.get('https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty');
-            console.log(res);
             newDetails.push(res.data);
             count++;
             if(count === post_count){
-                console.log("setData");
                 newDetails.map((data, igKey) =>{
                     const ele = <Post key={igKey + offset} title={data.title} link={data.url}/>
                     newList.push(ele);
                     return true;
-                    // return(
-                    //     <Post key={igKey} title={data.title} link={data.url}/>
-                    // )
                 })
-                console.log(newList);
                 setLoader(false);
                 setList([...newList]);
                 setOffset(post_count + offset);
